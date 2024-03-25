29074709510e8f2a7138544e56d2095af63fb65f
f2l5y <163734677+f2l5y@users.noreply.github.com>
2024-03-25 21:08:04 +0100

    Update salon.sh

--- salon.sh
+++ salon.sh
@@ -5,30 +5,18 @@ PSQL="psql -X --username=freecodecamp --dbname=salon --tuples-only -c"
 echo -e "\n~~~~~ MY SALON ~~~~~"
 echo -e "\nWelcome to My Salon, how can I help you?\n"
 
-MAIN_MENU() {
-if [[ $1 ]]
- then
-   echo -e "\n$1"
- fi
- echo "$($PSQL "SELECT * FROM services;")" | while IFS='|' read ID SERVICE
-  do
-	  if [[ $SERVICE != "name" ]]
-	  then
-		  echo "$ID) $SERVICE"
-          fi
-  done
-	read SERVICE_CHOICE
-		case $SERVICE_CHOICE in
-		1) SERVICE_ID_SELECTED=$SERVICE_CHOICE ;;
-		2) SERVICE_ID_SELECTED=$SERVICE_CHOICE ;;
-		3) SERVICE_ID_SELECTED=$SERVICE_CHOICE ;;
-		4) SERVICE_ID_SELECTED=$SERVICE_CHOICE ;;
-		5) SERVICE_ID_SELECTED=$SERVICE_CHOICE ;;
-		*) MAIN_MENU "I could not find that service. What would you like today?" ;;
-         	esac	
+MAIN_FUNCTION() {
+
+MAIN_MENU
 
 # get service name
-SERVICE_NAME=$($PSQL "SELECT name FROM services WHERE service_id=$SERVICE_ID_SELECTED")
+SERVICE_ID_SELECTED2=$($PSQL "SELECT name FROM services WHERE service_id=$SERVICE_NAME_SELECTED")
+
+SERVICE_ID_SELECTED2=$(echo $SERVICE_ID_SELECTED2 | sed -r 's/^ *| *$//g')
+
+#if a valid id has been selected, run the program
+if [[ -n $SERVICE_ID_SELECTED2 ]]
+then
 
 # get the customer number
 echo -e "\nWhat's your phone number?"
@@ -43,10 +31,11 @@ then
   read CUSTOMER_NAME  
 
   # Create a customer entry
-  $($PSQL "INSERT INTO customers(phone,name) VALUES('$CUSTOMER_PHONE','$CUSTOMER_NAME');")
+  RESULT_INSERT_CUSTOMER=$($PSQL "INSERT INTO customers(phone,name) VALUES('$CUSTOMER_PHONE','$CUSTOMER_NAME');")
+
 
   # Ask for a time
-  echo -e "\nWhat time would you like your cut, $CUSTOMER_NAME?"
+  echo -e "\nWhat time would you like your cut, $(echo $CUSTOMER_NAME | sed    -r 's/^ *| *$//g')?"
   read SERVICE_TIME
 
   # insert the customer appointment and service
@@ -54,10 +43,10 @@ then
    CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE name='$CUSTOMER_NAME'")
    
    #insert appointment data  
-  RESULT_APPOINTMENT=$($PSQL "INSERT INTO appointments(customer_id,service_id,time) VALUES($CUSTOMER_ID,$SERVICE_ID_SELECTED,'$SERVICE_TIME')")
+RESULT_INSERT_APP1=$($PSQL "INSERT INTO appointments(customer_id,service_id,time) VALUES($CUSTOMER_ID,$SERVICE_NAME_SELECTED,'$SERVICE_TIME')")
 
    #Communicate the data entered
-   echo -e "\nI have put you down for a $SERVICE_NAME at $SERVICE_TIME, $CUSTOMER_NAME."
+   echo -e "\nI have put you down for a $SERVICE_ID_SELECTED2 at $SERVICE_TIME, $(echo $CUSTOMER_NAME | sed    -r 's/^ *| *$//g')."
 
 else
    
@@ -65,21 +54,41 @@ else
 CUSTOMER_NAME=$($PSQL "SELECT name FROM customers WHERE phone='$CUSTOMER_PHONE';")
 
 # Get customer ID
-CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE name='$CUSTOMER_NAMEa';")
+CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone='$CUSTOMER_PHONE';")
 
   # Ask for a time
-  echo -e "\nWhat time would you like your cut, $CUSTOMER_NAME?"
+  echo -e "\nWhat time would you like your cut, $(echo $CUSTOMER_NAME | sed -r 's/^ *| *$//g')?"
   read SERVICE_TIME
 
-   
-   #insert appointment data  
-  RESULT_APPOINTMENT=$($PSQL "INSERT INTO appointments(customer_id,service_id,time) VALUES($CUSTOMER_ID,$SERVICE_ID_SELECTED,'$SERVICE_TIME')")
+  #insert appointment data  
+RESULT_INSERT_APP2=$($PSQL "INSERT INTO appointments(customer_id,service_id,time) VALUES($CUSTOMER_ID,$SERVICE_NAME_SELECTED,'$SERVICE_TIME');")
 
    #Communicate the data entered
-   echo -e "\nI have put you down for a$SERVICE_NAME at $SERVICE_TIME,$CUSTOMER_NAME."
-
+   echo -e "\nI have put you down for a $SERVICE_ID_SELECTED2 at $SERVICE_TIME, $(echo $CUSTOMER_NAME | sed -r 's/^ *| *$//g')."
 fi
-
+fi
+}
+MAIN_MENU() {
+if [[ $1 ]]
+ then
+   echo -e "\n$1"
+ fi
+ echo "$($PSQL "SELECT * FROM services;")" | while IFS='|' read ID SERVICE
+  do
+	  if [[ $SERVICE != "name" ]]
+	  then
+		  echo "$ID) $SERVICE"
+          fi
+  done
+	read SERVICE_ID_SELECTED
+		case $SERVICE_ID_SELECTED in
+		1) SERVICE_NAME_SELECTED=$SERVICE_ID_SELECTED ;;
+		2) SERVICE_NAME_SELECTED=$SERVICE_ID_SELECTED ;;
+		3) SERVICE_NAME_SELECTED=$SERVICE_ID_SELECTED ;;
+		4) SERVICE_NAME_SELECTED=$SERVICE_ID_SELECTED ;;
+		5) SERVICE_NAME_SELECTED=$SERVICE_ID_SELECTED ;;
+		*) MAIN_MENU "I could not find that service. What would you like today?" ;;
+         	esac	
 }
 
-MAIN_MENU
+MAIN_FUNCTION
